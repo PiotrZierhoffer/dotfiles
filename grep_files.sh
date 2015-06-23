264af37efb93365f8d50a2941b584f08568b0551
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2015-06-23 12:56:21 +0200

    Added volumeicon to xinitrc and grep_files

---
+++ grep_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -u
+
+IFS=$'\n'
+counter=0
+for entry in `grep -rIn --color=always "$1" .`
+do
+  echo "[$counter] $entry"
+
+  path=`echo $entry | cut -d':' -f1 | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'`
+  line=`echo $entry | cut -d':' -f2 | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'`
+
+  paths[$counter]=$path
+  lines[$counter]=$line
+
+  counter=$((counter + 1))
+done
+
+if [ "$counter" -gt 0 ]
+then
+  echo -n "Podaj numer wpisu, który ma zostać otwarty: "
+  read number
+
+  vim +${lines[$number]} ${paths[$number]}
+fi
